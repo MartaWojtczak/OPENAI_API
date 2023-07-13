6deeb49a573c54641a243ce767a96c7227d2e0b2
Marta Wojtczak <wojtczakmart@gmail.com>
2023-07-13 17:33:17 +0200

    add basic ChatCompletion feature

--- 4_code_and_gpt/CodeReviewer/reviewer.py
+++ 4_code_and_gpt/CodeReviewer/reviewer.py
@@ -1,6 +1,23 @@
 #basic idea is that I want to take a file and this script and receive
 #a review of a code in the file
 
+#libraries
+import openai
+from dotenv import load_dotenv
+import os
+
+#load API key to OPENAI products
+load_dotenv()
+openai.api_key = os.getenv("API")
+
+#code to be reviewed
+
+filecontent = """
+def function_to_be_reviewed(x, y):
+    return x**y
+"""
+
+#prompt message
 PROMPT = """
 You will receive a file's contents as text. 
 Generate a code review for the file. 
@@ -19,3 +36,18 @@ Be kind and constructive. Divide code review into two sections:
 
 For each suggested change, include line numbers to which you are reffering (if you do)
 """
+
+#messages
+
+messages = [
+{"role": "system", "content": PROMPT}
+, {"role": "user", "content": f"Code review the following file: {filecontent}"}
+]
+#ChatCompletion
+
+response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo"
+    , messages = messages
+)
+
+print(response["choices"][0]["message"]["content"])
